256738ee7cb996e4fb49d8970371644552757422
Elad Krispine <elad.krispine@gmail.com>
2022-09-12 15:55:28 +0300

    first commit

---
+++ src/components/Products.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import ProductCard from "./ProductCard.vue";
+
+export default defineComponent({
+  data() {
+    return {};
+  },
+  components: { ProductCard },
+});
+</script>
+
+<template>
+  <section class="products">
+    <ProductCard />
+    <ProductCard />
+    <ProductCard />
+    <ProductCard />
+    <ProductCard />
+    <ProductCard />
+    <ProductCard />
+    <ProductCard />
+    <ProductCard />
+    <ProductCard />
+    <ProductCard />
+  </section>
+</template>
+
+<style scoped>
+.products {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
